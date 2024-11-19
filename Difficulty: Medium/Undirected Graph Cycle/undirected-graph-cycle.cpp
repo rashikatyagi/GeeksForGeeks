be826f0c85c3b09415960d1716ce378f4a1c10d7
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-19 07:11:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -7,24 +7,35 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool bfsCycleDetect(int node, vector<vector<int>>& adj, unordered_map<int, int> &parent, vector<bool> &visited){
-        queue<int> q;
+    // bool bfsCycleDetect(int node, vector<vector<int>>& adj, unordered_map<int, int> &parent, vector<bool> &visited){
+    //     queue<int> q;
+    //     visited[node] = 1;
+    //     q.push(node);
+    //     parent[node] = -1;
+    //     while(!q.empty()) {
+    //         int currnode = q.front();
+    //         q.pop();
+    //         for(auto it : adj[currnode]){
+    //             if(visited[it] == 0){
+    //                 q.push(it);
+    //                 visited[it] = 1;
+    //                 parent[it] = currnode;
+    //             }
+    //             else if(parent[currnode] != it){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    bool dfsCycleDetect(int node, int parent, vector<vector<int>>& adj, vector<bool> &visited){
         visited[node] = 1;
-        q.push(node);
-        parent[node] = -1;
-        while(!q.empty()) {
-            int currnode = q.front();
-            q.pop();
-            for(auto it : adj[currnode]){
-                if(visited[it] == 0){
-                    q.push(it);
-                    visited[it] = 1;
-                    parent[it] = currnode;
-                }
-                else if(parent[currnode] != it){
-                    return true;
-                }
+        for(auto it : adj[node]){
+            if(visited[it] == 0){
+                bool ans = dfsCycleDetect(it, node, adj, visited);
+                if(ans) return true;
             }
+            else if(it != parent) return true;
         }
         return false;
     }
@@ -32,11 +43,11 @@ class Solution {
         // Code here
         int v = adj.size();
         vector<bool> visited(v, 0);
-        unordered_map<int, int> parent;
+        // unordered_map<int, int> parent;
         for(int i = 0 ; i < v ; i++){
             for(int j = 0 ; j < adj[i].size() ; j++){
                 if(visited[adj[i][j]] == 0){
-                    bool ans = bfsCycleDetect(adj[i][j], adj, parent, visited);
+                    bool ans = dfsCycleDetect(adj[i][j], -1, adj, visited);
                     if(ans) return true;
                 }
             }
@@ -44,6 +55,7 @@ class Solution {
         return false;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
