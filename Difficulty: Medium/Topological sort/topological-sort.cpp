d5263acffac781d7beed03f6fa014caaa04872f6
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-22 14:36:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -5,33 +5,64 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  public:
+  public: 
     // Function to return list containing vertices in Topological order.
-    void dfs(int source, vector<vector<int>>& adj, vector<bool> &visited, stack<int> &st){
-        visited[source] = 1;
-        for(auto it : adj[source]){
-            if(visited[it] == 0){
-                dfs(it, adj, visited, st);
+    // void dfs(int source, vector<vector<int>>& adj, vector<bool> &visited, stack<int> &st){
+    //     visited[source] = 1;
+    //     for(auto it : adj[source]){
+    //         if(visited[it] == 0){
+    //             dfs(it, adj, visited, st);
+    //         }
+    //     }
+    //     st.push(source);
+    // }
+    void bfs(vector<vector<int>>& adj, vector<int> &ans){
+        int v = adj.size();
+        vector<int> indegree(v, 0);
+        //get the indegrees of all the vertices
+        for(int i = 0 ; i < v ; i++){
+            for(int j = 0 ; j < adj[i].size() ; j++){
+                indegree[adj[i][j]]++;
             }
         }
-        st.push(source);
+        //push all the vertices with 0 indegree into the queue
+        queue<int> q;
+        for(int i = 0 ; i < v ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node = q.front();
+            ans.push_back(node);
+            q.pop();
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
     }
     vector<int> topologicalSort(vector<vector<int>>& adj) {
         // Your code here
-        int v = adj.size();
-        vector<bool> visited(v, 0);
-        stack<int> st;
-        for(int node = 0 ; node < v ; node++){
-            if(visited[node] == 0){
-                dfs(node, adj, visited, st);
-            }
-        }
-        // remove the elements from the stack
+        // int v = adj.size();
+        // vector<bool> visited(v, 0);
+        // stack<int> st;
+        // for(int node = 0 ; node < v ; node++){
+        //     if(visited[node] == 0){
+        //         dfs(node, adj, visited, st);
+        //     }
+        // }
+        // // remove the elements from the stack
+        // vector<int> answer;
+        // while(!st.empty()){
+        //     answer.push_back(st.top());
+        //     st.pop();
+        // }
+        // return answer;
         vector<int> answer;
-        while(!st.empty()){
-            answer.push_back(st.top());
-            st.pop();
-        }
+        bfs(adj, answer);
         return answer;
     }
 };
